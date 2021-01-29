ee712d0388667fa7c7475a5760bb9cf2b9dfd03e
Tomasz Posiadala <tomaszposiadala@gmail.com>
2021-01-29 20:51:01 +0000

    adding collection item component

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
-import './collection-preview.styles.scss';
+import './collection-preview.styles.scss'
+
+import CollectionItem from '../collection-item/collection-item.component'
 
 const CollectionPreview = ({ title, items }) => (
   <div className='collection-preview'>
     <h1 className='title'>{title.toUpperCase()}</h1>
     <div className='preview'>
-      {items.map(item => (
-          <div key={item.id}>{item.name}</div>
+      {items.filter((item, idx) => idx < 4).map(({id, ...otherItemProps}) => (
+          <CollectionItem key={id} {...otherItemProps}/>
         ))}
     </div>
   </div>
