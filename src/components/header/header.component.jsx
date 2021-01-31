fab108ab403521ae590dc751001dbdd21d4df0eb
Tomasz Posiadala <tomaszposiadala@gmail.com>
2021-01-31 20:53:47 +0000

    added sign in/out button to the header that reads value from app.js state, styled signinwithgoogle button

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,7 +5,10 @@ import { ReactComponent as Logo } from '../../assets/crown.svg'
 
 import './header.styles.scss';
 
-const Header = () => (
+//firebase
+import { auth } from '../../firebase/firebase.utils';
+
+const Header = ({ currentUser }) => (
     <div className='header'>
         <Link className='logo-container' to="/">
             <Logo className='logo'/>
@@ -13,6 +16,12 @@ const Header = () => (
         <div className='options'>
             <Link className='option' to='/shop'>SHOP</Link>
             <Link className='option' to='/shop'>CONTACT</Link>
+            {
+                currentUser ? (
+                <div class='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+                ) : (
+                <Link className='option' to='/signin'>SIGN IN</Link>)
+            }
         </div>
     </div>
 )
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -47,9 +47,10 @@ class SignIn extends React.Component {
                         handleChange={this.handleChange}
                         label="password" 
                         required/>
-
-                    <CustomButton type="submit"> SIGN IN </CustomButton>
-                    <CustomButton onClick={signInWithGoogle}> SIGN IN WITH GOOGLE </CustomButton>
+                    <div className='buttons'>
+                        <CustomButton type="submit"> SIGN IN </CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn> SIGN IN WITH GOOGLE </CustomButton>
+                    </div>
                 </form>
             </div>
         )
